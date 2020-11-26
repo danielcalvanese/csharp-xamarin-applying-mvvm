48dac6563da970203930d0159d44a6fb0cddb688
Daniel Calvanese <Daniel.Calvanese@Gmail.com>
2020-11-26 18:48:31 -0500

    Added module 3 tasks.

---
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Roster.Client.ViewModels
+{
+    public partial class HomeViewModel : INotifyPropertyChanged
+    {
+        public string Title { get; set; } = "Roster App";
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Command UpdateApplicationCommand { get; }
+    }
+}
