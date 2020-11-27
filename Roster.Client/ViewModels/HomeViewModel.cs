b8dd51821b251499b42f2d420c30038c3ff6d99a
Daniel Calvanese <Daniel.Calvanese@Gmail.com>
2020-11-26 19:34:42 -0500

    Added module 4 tasks.

--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -8,9 +8,24 @@ namespace Roster.Client.ViewModels
 {
     public partial class HomeViewModel : INotifyPropertyChanged
     {
-        public string Title { get; set; } = "Roster App";
+        public HomeViewModel()
+        {
+            UpdateApplicationCommand = new Command(UpdateApplicationExecute);
+        }
+
+        private string _title = "Roster App";
+        public string Title {
+            get { return _title; }
+            set {
+                _title = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Title)));
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
         public Command UpdateApplicationCommand { get; }
+        private void UpdateApplicationExecute() {
+            Title = "Roster App (v2.0)";
+        }
     }
 }
